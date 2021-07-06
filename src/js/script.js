96afaa074a22ea77ec6aa39558921b246e3e06e3
Alexander Botwinick <alexbotwinick@gmail.com>
2021-07-06 23:40:25 +0200

    script refactor, sticky nav logic

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,39 @@
 import { projects } from './projects.js';
-import { capitalize } from './helpers.js';
+import { renderProjectCard } from './views/projectView.js';
+import { updateTheme } from './updateTheme.js';
 
 const projectContainer = document.querySelector('.projects__container');
-
-const renderProjectCard = function (project) {
-  const html = `
-    <div class="project project-card project__${project.title}">
-          <img
-            class="project-card__image"
-            src="src/images/${project.imagePath}"
-            alt="${project.imageDescription}"
-          />
-          <div class="project-card__infos">
-            <div class="project-card__title">${capitalize(project.title)}</div>
-            <div class="project-card__description">
-              <p>${project.description}</p>
-            </div>
-            <div class="project-card__footer">
-              <span>${project.languages}</span>
-              <a
-                href="${project.url}"
-                class="project-card__link"
-                target="_blank"
-                >Visit 🚀</a
-              >
-            </div>
-          </div>
-        </div>
-  `;
-
-  projectContainer.insertAdjacentHTML('beforeend', html);
-};
+const themeToggleInput = document.querySelector('.toggle__input');
 
 // iterate over projects and render each card
-projects.forEach(project => renderProjectCard(project));
+
+//*************DARK AND LIGHT THEME LOGIC*************
+// user preference in browser?
+const userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+if (userPrefersDark.matches) {
+  themeToggleInput.checked = 'true';
+  updateTheme('dark');
+}
+
+themeToggleInput.addEventListener('click', e => {
+  const themeToggledDark = e.target.checked; // returns true for dark, false for light
+  themeToggledDark ? updateTheme('dark') : updateTheme('light');
+});
+
+// ***********sticky nav with opacity on scroll /// **************************
+const nav = document.querySelector('.nav');
+console.log('nav', nav.offsetHeight);
+
+window.addEventListener('scroll', function (e) {
+  const scrollY = this.scrollY;
+  if (scrollY > nav.offsetHeight) {
+    nav.style.position = 'sticky';
+    nav.style.top = '0';
+    nav.style.opacity = '.8';
+  } else {
+    nav.style.opacity = '1';
+  }
+});
+
+// Dynamically add projects -- Commented out because the flip effect needed both sides to have consisent heights, which was difficult when considering various image aspect ratios and orientations
+// projects.forEach(project => renderProjectCard(project, projectContainer));
