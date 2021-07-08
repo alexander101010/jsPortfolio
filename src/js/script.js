950a7317bee40dcabf1c5b4d6f8e54feccabfe95
Alexander Botwinick <alexbotwinick@gmail.com>
2021-07-08 14:57:52 +0200

    removed sticky nav logic, added theme toggle position adjustment on scroll

--- src/js/script.js
+++ src/js/script.js
@@ -20,20 +20,21 @@ themeToggleInput.addEventListener('click', e => {
   themeToggledDark ? updateTheme('dark') : updateTheme('light');
 });
 
-// ***********sticky nav with opacity on scroll /// **************************
+// ***********
 const nav = document.querySelector('.nav');
-console.log('nav', nav.offsetHeight);
+const toggleBox = document.querySelector('.toggle__box');
 
 window.addEventListener('scroll', function (e) {
   const scrollY = this.scrollY;
   if (scrollY > nav.offsetHeight) {
-    nav.style.position = 'sticky';
-    nav.style.top = '0';
-    nav.style.opacity = '.8';
-  } else {
-    nav.style.opacity = '1';
+    toggleBox.style.top = '0';
+  }
+  if (scrollY < nav.offsetHeight) {
+    toggleBox.style.top = '8rem';
   }
 });
 
 // Dynamically add projects -- Commented out because the flip effect needed both sides to have consisent heights, which was difficult when considering various image aspect ratios and orientations
-// projects.forEach(project => renderProjectCard(project, projectContainer));
+projects.forEach(project => renderProjectCard(project, projectContainer));
+
+// Move theme toggle up into right top corner when
