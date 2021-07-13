aefdfd055c96ca3b7d6e7d6bd400eb432c1349a2
Alexander Botwinick <alexbotwinick@gmail.com>
2021-07-13 22:29:12 +0200

    smoothScroll and uncheckInput helper functions

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -2,3 +2,13 @@
 export const capitalize = function (word) {
   return word[0].toUpperCase() + word.slice(1).toLowerCase();
 };
+
+export const smoothScroll = function (buttonElement, scrollTarget) {
+  buttonElement.addEventListener('click', e => {
+    scrollTarget.scrollIntoView({ behavior: 'smooth' });
+  });
+};
+
+export const uncheckInput = element => {
+  element.checked = false;
+};
