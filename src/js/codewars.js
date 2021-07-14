36e8e7f016cb07608184016f034aac1f888932a4
Alexander Botwinick <alexbotwinick@gmail.com>
2021-07-14 22:23:24 +0200

    started to play with scraping solutions from codewars problems completed

---
+++ src/js/codewars.js
@@ -0,0 +1,10 @@
+const scrapeTarget =
+  'https://www.codewars.com/users/alexander101010/completed_solutions';
+
+// function ipToInt32(ip) {
+//   const binaries = ip
+//     .split('.')
+//     .map(strNum => Number(strNum).toString(2).padStart(8, '0'));
+
+//   return parseInt(binaries.join(''), 2);
+// }
