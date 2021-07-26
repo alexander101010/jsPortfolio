27ef6613240f11e2d3e66b5161ab40e5b3de7e9b
Alexander Botwinick <alexbotwinick@gmail.com>
2021-07-26 18:46:07 +0200

    script cleanup, move consts to top of file

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,27 @@ import { renderProjectCard } from './views/projectView.js';
 import { updateTheme } from './updateTheme.js';
 import { smoothScroll, uncheckInput } from './helpers.js';
 
+const nav = document.querySelector('#nav');
+const mobileNavLinks = document.querySelectorAll('.mobile-nav__link');
+const navInput = document.querySelector('#navi-toggle');
+
 const projectContainer = document.querySelector('.projects__container');
+const toggleBox = document.querySelector('.toggle__box');
 const themeToggleInput = document.querySelector('.toggle__input');
+const scrollToTopBtn = document.querySelector('.scroll-to-top__box');
+
+const alexAvatar = document.querySelector('.banner__photo');
+const navProjectsBtn = document.querySelector('#nav__btn-projects');
+const navAboutBtn = document.querySelector('#nav__btn-about');
+
+const scrollProjectsBtn = document.querySelector('.scroll-down');
+const footerProjectsLink = document.querySelector('#footer-projects-link');
+
+//SECTIONS
+const aboutMeSection = document.querySelector('#section-about');
+const projectsSection = document.querySelector('#section-projects');
+
+const sections = { about: aboutMeSection, projects: projectsSection };
 
 // *************************************************
 //*************DARK AND LIGHT THEME LOGIC*************
@@ -25,9 +44,6 @@ themeToggleInput.addEventListener('click', e => {
 // *************************************************
 // *********** THEME TOGGLE / scrolltoTop ANIMATION **************
 // *************************************************
-const nav = document.querySelector('#nav');
-const toggleBox = document.querySelector('.toggle__box');
-const scrollToTopBtn = document.querySelector('.scroll-to-top__box');
 smoothScroll(scrollToTopBtn, nav);
 
 window.addEventListener('scroll', function (e) {
@@ -52,9 +68,9 @@ projects.forEach(project => renderProjectCard(project, projectContainer));
 // ***************************************************************
 // Smooth scrolling using helper function (takes btnElement and targetScrollElement) ***
 // **************************************************************
-const scrollProjectsBtn = document.querySelector('.scroll-down');
-const projectsSection = document.querySelector('#section-projects');
-const footerProjectsLink = document.querySelector('#footer-projects-link');
+// const scrollProjectsBtn = document.querySelector('.scroll-down');
+// const projectsSection = document.querySelector('#section-projects');
+// const footerProjectsLink = document.querySelector('#footer-projects-link');
 footerProjectsLink.addEventListener('click', e => {
   e.preventDefault(); // prevent a-tag from moving to section right away, allowing for smooth scroll instead
 });
@@ -62,14 +78,12 @@ footerProjectsLink.addEventListener('click', e => {
 smoothScroll(scrollProjectsBtn, projectsSection);
 smoothScroll(footerProjectsLink, projectsSection);
 
-const alexAvatar = document.querySelector('.banner__photo');
-const aboutMeSection = document.querySelector('#section-about');
+// const alexAvatar = document.querySelector('.banner__photo');
+// const aboutMeSection = document.querySelector('#section-about');
+// const navProjectsBtn = document.querySelector('#nav__btn-projects');
+// const navAboutBtn = document.querySelector('#nav__btn-about');
 
 smoothScroll(alexAvatar, aboutMeSection);
-
-const navProjectsBtn = document.querySelector('#nav__btn-projects');
-const navAboutBtn = document.querySelector('#nav__btn-about');
-
 smoothScroll(navProjectsBtn, projectsSection);
 smoothScroll(navAboutBtn, aboutMeSection);
 
@@ -77,11 +91,22 @@ smoothScroll(navAboutBtn, aboutMeSection);
 // Hide Overlay on mobile hamburger button after one of the selections is clicked
 // *************************************************************
 
-const mobileNavLinks = document.querySelectorAll('.mobile-nav__link');
-const navInput = document.querySelector('#navi-toggle');
+// const mobileNavLinks = document.querySelectorAll('.mobile-nav__link');
+// const navInput = document.querySelector('#navi-toggle');
+
+const mobileHamProjectsBtn = document.querySelector(
+  '.mobile-nav__link-projects'
+);
+const mobileHamAboutBtn = document.querySelector('.mobile-nav__link-about');
+
+smoothScroll(mobileHamProjectsBtn, projectsSection);
+smoothScroll(mobileHamAboutBtn, aboutMeSection);
 
 mobileNavLinks.forEach(link => {
-  link.addEventListener('click', () => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
     uncheckInput(navInput);
+    // smoothScroll(link, sections[link.dataset.section]);
+    // // sections[link.dataset.section].scrollIntoView({ behavior: 'smooth' });
   });
 });
